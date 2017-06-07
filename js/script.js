2277f84be2b4a875e32bd560dee9974d9d90fbc1
emilio <lcalderon@b7-05.local>
2017-06-07 10:06:17 -0600

    Form Changes

--- js/script.js
+++ js/script.js
@@ -27,10 +27,12 @@ var domIsReady = (function(domIsReady) {
 (function(document, window, domIsReady, undefined) {
   domIsReady(function() {
      var questionBox = document.getElementById('question-box');
+     var questionTitle = document.getElementById('question-title');
 
-     questionBox.addEventListener('click', function() {
+     questionTitle.addEventListener('click', function() {
       if (questionBox.classList) {
         questionBox.classList.toggle('is-open');
+        //phoneRequired.setAttribute('required', 'yes');
       } else {
         var classes = questionBox.className.split(' ');
         var existingIndex = classes.indexOf('is-open');
@@ -43,5 +45,76 @@ var domIsReady = (function(domIsReady) {
         questionBox.className = classes.join(' ');
       }
     });
+
+    var boxPhone = document.getElementById('box-phone');
+    var inputPhone = document.getElementById('input-phone');
+    var phoneRequired = document.getElementById('phone-required');
+
+    boxPhone.addEventListener('click', function () {
+      if (boxPhone.checked) {
+        inputPhone.classList.toggle('input-show');
+        phoneRequired.setAttribute('required', 'yes');
+
+      } else {
+        var classes = inputPhone.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+        phoneRequired.removeAttribute('required');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputPhone.className = classes.join(' ');
+      }
+    });
+
+    var boxMail = document.getElementById('box-mail');
+    var inputMail = document.getElementById('input-mail');
+    var mailRequired = document.getElementById('mail-required');
+
+    boxMail.addEventListener('click', function () {
+      if (boxMail.checked) {
+        inputMail.classList.toggle('input-show');
+        mailRequired.setAttribute('required', 'yes');
+
+      } else {
+        var classes = inputMail.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+        mailRequired.removeAttribute('required');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputMail.className = classes.join(' ');
+      }
+    });
+    
+    var boxLetter = document.getElementById('box-letter');
+    var inputLetter = document.getElementById('input-letter');
+    var letterRequired = document.getElementById('letter-required');
+
+    boxLetter.addEventListener('click', function () {
+      if (boxLetter.checked) {
+        inputLetter.classList.toggle('input-show');
+        letterRequired.setAttribute('required', 'yes');
+
+      } else {
+        var classes = inputLetter.className.split(' ');
+        var existingIndex = classes.indexOf('input-show');
+        letterRequired.removeAttribute('required');
+
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push('input-show');
+
+        inputLetter.className = classes.join(' ');
+      }
+    });
+
+
   });
 })(document, window, domIsReady);
